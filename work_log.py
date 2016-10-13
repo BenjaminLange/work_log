16e66ff42311145c4e033be817c861245de76534
= <benjamin.r.lange@gmail.com>
2016-10-12 21:42:49 -0700

    Add constant variables for filenames, time formats, and fieldnames

--- work_log.py
+++ work_log.py
@@ -7,7 +7,11 @@ from datetime import datetime
 from datetime import timedelta
 
 
-work_log_filename = "work_log.csv"
+WORK_LOG_FILENAME = 'work_log.csv'
+TEMP_WORK_LOG_FILENAME = 'temp_work_log.csv'
+FMT_MONTH_DAY_YEAR = '%m/%d/%y'
+FMT_HOUR_MINUTE = '%H:%M'
+FIELDNAMES = ['id', 'name', 'date', 'time_spent', 'notes']
 
 
 def start():
@@ -37,14 +41,13 @@ def new_entry():
     add_notes = input("Add notes? Y/n ").lower()
     if add_notes != 'n':
         entry['notes'] = input_notes()
-    entry['date'] = datetime.now().strftime('%m/%d/%y')
-    fieldnames = ['id', 'name', 'date', 'time_spent', 'notes']
-    with open(work_log_filename, 'a', newline='') as work_log:
-        work_log_writer = csv.DictWriter(work_log, fieldnames=fieldnames)
+    entry['date'] = datetime.now().strftime(FMT_MONTH_DAY_YEAR)
+    with open(WORK_LOG_FILENAME, 'a', newline='') as work_log:
+        work_log_writer = csv.DictWriter(work_log, fieldnames=FIELDNAMES)
         work_log_writer.writerow(entry)
 
 
-def input_name(update=False):
+def input_name():
     name = input("Task Name: ")
     return name
 
@@ -67,7 +70,7 @@ def input_time_spent(update=False):
             return input_time_spent()
 
 
-def input_notes(update=False):
+def input_notes():
     note_list = []
     notes = "A"
     print("Notes:")
@@ -80,14 +83,15 @@ def input_notes(update=False):
 
 def input_date(update=False):
     date = input("Date (MM/DD/YY): ")
-    if date == '':
-        return ''
+    if update:
+        if date == '':
+            return ''
     try:
-        date = datetime.strptime(date, '%m/%d/%y')
+        date = datetime.strptime(date, FMT_MONTH_DAY_YEAR)
     except ValueError:
         print("I don't recognize that format, please try again.")
         return input_date()
-    date = date.strftime('%m/%d/%y')
+    date = date.strftime(FMT_MONTH_DAY_YEAR)
     return date
 
 
@@ -124,10 +128,11 @@ def search_by_time_spent(error=None):
         minimum = convert_string_to_timedelta(time_list[0].strip())
         maximum = convert_string_to_timedelta(time_list[1].strip())
         entries = []
-        with open(work_log_filename, 'r') as work_log:
+        with open(WORK_LOG_FILENAME, 'r') as work_log:
             work_log_reader = csv.DictReader(work_log)
             for entry in work_log_reader:
-                entry_time = convert_string_to_timedelta(entry['time_spent'], '%H:%M:%S')
+                entry_time = convert_string_to_timedelta(
+                    entry['time_spent'], '%H:%M:%S')
                 if minimum <= entry_time <= maximum:
                     entries.append(entry)
         print_entries(entries)
@@ -138,7 +143,7 @@ def search_by_time_spent(error=None):
     else:
         error = "I don't recognize that format. Please try again.\n"
         return search_by_time_spent(error)
-    with open(work_log_filename, 'r') as work_log:
+    with open(WORK_LOG_FILENAME, 'r') as work_log:
         entries = []
         work_log_reader = csv.DictReader(work_log)
         for entry in work_log_reader:
@@ -158,21 +163,24 @@ def search_by_date(error=None):
     date = re.compile(r'\d\d/\d\d/\d\d')
     if date_range.search(user_input):
         date_list = user_input.split('-')
-        start = datetime.strptime(date_list[0].strip(), '%m/%d/%y')
-        end = datetime.strptime(date_list[1].strip(), '%m/%d/%y')
+        start_date = datetime.strptime(
+            date_list[0].strip(), FMT_MONTH_DAY_YEAR)
+        end_date = datetime.strptime(
+            date_list[1].strip(), FMT_MONTH_DAY_YEAR)
         entries = []
-        with open(work_log_filename, 'r') as work_log:
+        with open(WORK_LOG_FILENAME, 'r') as work_log:
             work_log_reader = csv.DictReader(work_log)
-            for idx, entry in enumerate(work_log_reader):
-                entry_date = datetime.strptime(entry['date'], '%m/%d/%y')
-                if start <= entry_date <= end:
+            for entry in work_log_reader:
+                entry_date = datetime.strptime(
+                    entry['date'], FMT_MONTH_DAY_YEAR)
+                if start_date <= entry_date <= end_date:
                     entries.append(entry)
         print_entries(entries)
     elif date.search(user_input):
         entries = []
-        with open(work_log_filename, 'r') as work_log:
+        with open(WORK_LOG_FILENAME, 'r') as work_log:
             work_log_reader = csv.DictReader(work_log)
-            for idx, entry in enumerate(work_log_reader):
+            for entry in work_log_reader:
                 if user_input == entry['date']:
                     entries.append(entry)
         print_entries(entries)
@@ -186,7 +194,7 @@ def search_by_string():
     clear_screen()
     user_input = input("What would you like to search for? ").lower()
     entries = []
-    with open(work_log_filename, 'r') as work_log:
+    with open(WORK_LOG_FILENAME, 'r') as work_log:
         work_log_reader = csv.DictReader(work_log)
         for entry in work_log_reader:
             if(user_input in entry['name'].lower() or
@@ -198,13 +206,14 @@ def search_by_string():
 def search_by_pattern():
     clear_screen()
     user_input = input("What pattern would you like to search for? ")
-    search = re.compile(user_input)
+    search_pattern = re.compile(user_input)
     entries = []
-    with open(work_log_filename, 'r') as work_log:
+    with open(WORK_LOG_FILENAME, 'r') as work_log:
         work_log_reader = csv.DictReader(work_log)
         for entry in work_log_reader:
             try:
-                if search.search(entry['name']) or search.search(entry['notes']):
+                if (search_pattern.search(entry['name']) or
+                        search_pattern.search(entry['notes'])):
                     entries.append(entry)
             except IndexError:
                 pass
@@ -270,21 +279,20 @@ def print_entries(entries):
                 error = "End of list. Can't move forward."
 
 
-def delete_entry(id):
-    with open(work_log_filename, 'r') as work_log:
-        fieldnames = ['id', 'name', 'date', 'time_spent', 'notes']
-        copy = open('temp_work_log.csv', 'a', newline='')
-        copy_writer = csv.DictWriter(copy, fieldnames=fieldnames)
+def delete_entry(entry_id):
+    with open(WORK_LOG_FILENAME, 'r') as work_log:
+        copy = open(TEMP_WORK_LOG_FILENAME, 'a', newline='')
+        copy_writer = csv.DictWriter(copy, fieldnames=FIELDNAMES)
         copy_writer.writeheader()
         work_log_reader = csv.DictReader(work_log)
         for entry in work_log_reader:
-            if entry['id'] == id:
+            if entry['id'] == entry_id:
                 continue
             else:
                 copy_writer.writerow(entry)
         copy.close()
-    os.remove(work_log_filename)
-    os.rename('temp_work_log.csv', work_log_filename)
+    os.remove(WORK_LOG_FILENAME)
+    os.rename(TEMP_WORK_LOG_FILENAME, WORK_LOG_FILENAME)
 
 
 def edit_entry(entry):
@@ -293,19 +301,18 @@ def edit_entry(entry):
     prev_date = entry['date']
     prev_time_spent = entry['time_spent']
     prev_notes = entry['notes']
-    new_name = input_name(update=True)
+    new_name = input_name()
     entry['name'] = new_name or prev_name
     new_date = input_date(update=True)
     entry['date'] = new_date or prev_date
     new_time_spent = input_time_spent(update=True)
     entry['time_spent'] = new_time_spent or prev_time_spent
-    new_notes = input_notes(update=True)
+    new_notes = input_notes()
     entry['notes'] = new_notes or prev_notes
 
-    with open(work_log_filename, 'r') as work_log:
-        fieldnames = ['id', 'name', 'date', 'time_spent', 'notes']
-        copy = open('temp_work_log.csv', 'a', newline='')
-        copy_writer = csv.DictWriter(copy, fieldnames=fieldnames)
+    with open(WORK_LOG_FILENAME, 'r') as work_log:
+        copy = open(TEMP_WORK_LOG_FILENAME, 'a', newline='')
+        copy_writer = csv.DictWriter(copy, fieldnames=FIELDNAMES)
         copy_writer.writeheader()
         work_log_reader = csv.DictReader(work_log)
         for prev_entry in work_log_reader:
@@ -314,42 +321,42 @@ def edit_entry(entry):
             else:
                 copy_writer.writerow(prev_entry)
         copy.close()
-    os.remove(work_log_filename)
-    os.rename('temp_work_log.csv', work_log_filename)
+    os.remove(WORK_LOG_FILENAME)
+    os.rename(TEMP_WORK_LOG_FILENAME, WORK_LOG_FILENAME)
 
 
-def convert_string_to_timedelta(time, fmt='%H:%M'):
+def convert_string_to_timedelta(time, fmt=FMT_HOUR_MINUTE):
     time_range = datetime.strptime(time, fmt)
-    time_range = timedelta(hours=time_range.hour,
-                           minutes=time_range.minute,
-                           seconds=time_range.second)
-    return time_range
+    time_range_delta = timedelta(hours=time_range.hour,
+                                 minutes=time_range.minute,
+                                 seconds=time_range.second)
+    return time_range_delta
 
 
-def convert_minutes_to_timedelta(minutes):
-    if int(minutes) >= 60:
-        minutes = int(minutes)
+def convert_minutes_to_timedelta(minutes_string):
+    minutes = int(minutes_string)
+    if minutes >= 60:
         hours = int(minutes / 60)
         minutes = minutes % 60
-        minutes = "{}:{}".format(hours, minutes)
-        minutes = datetime.strptime(minutes, '%H:%M')
+        minutes_string = "{}:{}".format(hours, minutes)
+        minutes_date_time = datetime.strptime(minutes, FMT_HOUR_MINUTE)
     else:
-        minutes = datetime.strptime(minutes, '%M')
-    minutes = timedelta(hours=minutes.hour,
-                        minutes=minutes.minute,
-                        seconds=minutes.second)
-    return minutes
+        minutes_date_time = datetime.strptime(minutes, '%M')
+    minutes_delta = timedelta(hours=minutes_date_time.hour,
+                              minutes=minutes_date_time.minute,
+                              seconds=minutes_date_time.second)
+    return minutes_delta
 
 
 def get_next_id():
-    with open(work_log_filename, 'r') as work_log:
+    with open(WORK_LOG_FILENAME, 'r') as work_log:
         work_log_reader = csv.DictReader(work_log)
-        id = 0
+        entry_id = 0
         for entry in work_log_reader:
-            if int(entry['id']) > id:
-                id = int(entry['id'])
-        id += 1
-        return id
+            if int(entry['id']) > entry_id:
+                entry_id = int(entry['id'])
+        entry_id += 1
+        return entry_id
 
 
 def clear_screen():
